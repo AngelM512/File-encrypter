6cf1a259c32fe72647a245333f8a16fa6a15b587
Angel Moreta <37674133+rundacode@users.noreply.github.com>
2019-12-18 10:47:36 -0600

    Update encrypt.cpp

--- encrypt.cpp
+++ encrypt.cpp
@@ -7,62 +7,51 @@
 #include <string>
 using namespace std;
 
+// function prototypes
 void clearScreen();
-void menu(string &fileName);
+char menu(string &fileName);
 void encryptFile(fstream &input, fstream &output, int);
-void decryptFile(fstream &input, fstream &output, int);
+void decryptFile(fstream &input, fstream &output);
 
 int main(){
     const int ENCRYPT = 10;
+    char choice;
     string fileName;
     const string fileEncryptedName = "encrypted.txt"; 
+    const string fileDescrypt = "descrypted.txt";
 
-    menu(fileName);
-    //create file object + open file 
-    fstream fileFilter(fileName, ios::in);
-    fstream encryptedFile(fileEncryptedName, ios::out);
+    // display menu + return a char
+    choice = menu(fileName);
 
+    //create file object 
+    fstream fileFilter;
+    fstream encryptedFile;
+    //doesFileExist?
     
-    // verify if file is in directory
-    clearScreen();
-    if(!fileFilter)
+    clearScreen();  
+    switch (choice)
     {
-        cout << "\n\n** File was not found, program has been terminated **\n\n";
-        exit(1);
-    } else {
-        // encrypt the file and save in a .txt file
-        encryptFile( fileFilter, encryptedFile,  ENCRYPT );
-        cout << "\nfilename --> " << fileName << " <--  was encrypted" << endl;
-        cout <<"***************************************";
-        cout <<"\n\n\n\n\n";
-        cout << "** Encrypted file was saved as: " << fileEncryptedName << endl; 
-        cout <<"\n\n\n\n\n";
-        cout <<"***************************************" << endl;
+    case 'a':
+        fileFilter.open(fileName, ios::in);
+        encryptedFile.open(fileEncryptedName, ios::out);
+        /* call function */
+        encryptFile( fileFilter, encryptedFile, ENCRYPT);
+        break;
+    case 'b':
+        encryptedFile.open(fileEncryptedName, ios::in);
+        fileFilter.open(fileDescrypt, ios::out);
+        /* call function */
+        decryptFile(encryptedFile, fileFilter);
+        break;
+    
     }
-    // close files
+
+    // close files, end program
     fileFilter.close();
     encryptedFile.close();
+    cout << "File has been closed\n" << endl;
     return 0;
 };
-
-void clearScreen(){ // clear the screen 
-    cout << string( 100, '\n' );
-}
-
-void menu(string &fileName){
-    char choice;
-    cout << "* Welcome to encrypter by Angel Moreta *" << endl; // welcome user
-    cout << "( A ) to encrypt a .txt file | ( B ) to descrypt a .txt file: ";
-    cin >> choice;
-    
-    // RIGHT NOW WORKING WITH OPTION A
-    if (tolower(choice) == 'a'){
-        // search for file 
-        cout << "\nEnter file you wish to encrypt: ";
-        cin >> fileName;
-    };
-
-}
 
 // pass file as a ref
 void encryptFile(fstream &input, fstream &output, int ENCRYPT){
@@ -70,10 +59,59 @@ void encryptFile(fstream &input, fstream &output, int ENCRYPT){
     input.get(content); // first char of the file
     
     // as long file dont end; loop
-    while (!input.fail())
+    while ( !input.fail() )
     {
        content += ENCRYPT;
        output.put(content); // put in encrypted.txt the encrypted content.
        input.get(content);
     }
+    cout << "\n\n\n\n\n\n\n\n";
+    cout << "File was encrypted and save as --> encrypted.txt" << endl;
 }
+
+void decryptFile(fstream &input, fstream &output)
+{
+    char content;
+    input.get(content);
+    // as long file dont end; loop
+    while ( !input.fail() )
+    {
+       content -= 10;
+       output.put(content);
+       input.get(content);
+    };
+    cout << "\n\n\n\n\n\n\n\n";
+    cout << "File was decrypted and save as --> decrypted.txt" << endl;
+
+}
+
+void clearScreen()
+{ 
+    // clear the screen 
+    cout << string( 100, '\n' );
+}
+
+char menu(string &fileName) 
+{
+    char choice;
+    cout << "\n\n* Welcome to encrypter by Angel Moreta *" << endl; // welcome user
+    cout << "( A ) to encrypt a .txt file | ( B ) to descrypt a .txt file: ";
+    cin >> choice;
+
+    if ( tolower( choice ) == 'a' )
+    {
+        cout << "Enter file file: ";
+        cin >> fileName;
+    } else if ( tolower( choice ) == 'b' )
+    {
+        return choice;
+    } 
+    else
+    {
+        cout << "\n\n* Functionality not available *\n\n";
+        exit(1);
+    }
+    
+    return tolower( choice );
+};
+
